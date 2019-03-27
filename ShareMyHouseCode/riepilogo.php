03e290d25dd31d510bf89b800fe1557b2f7e3378
garjilius <emanuelegar@gmail.com>
2019-03-27 18:51:27 +0100

    Cambiato titolo barra

--- ShareMyHouseCode/riepilogo.php
+++ ShareMyHouseCode/riepilogo.php
@@ -35,7 +35,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>                        
                     </button>
-                    <a class="navbar-brand" href="riepilogo.php">CittadinAPP</a>
+                    <a class="navbar-brand" href="riepilogo.php">ShareMyHouse</a>
                 </div>
                 <div class="collapse navbar-collapse" id="myNavbar">
                     <ul class="nav navbar-nav bottoniStato">
