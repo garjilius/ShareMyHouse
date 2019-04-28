4ae19dfd814a80eaff6197e9612337d8c267225e
garjilius <emanuelegar@gmail.com>
2019-04-28 21:55:13 +0200

    microFix pagina errore login

--- ShareMyHouseCode/errorPage.php
+++ ShareMyHouseCode/errorPage.php
@@ -33,7 +33,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>                        
                     </button>
-                    <a class="navbar-brand">CittadinAPP</a>
+                    <a class="navbar-brand">ShareMyHouse</a>
                 </div>
                 <div class="collapse navbar-collapse" id="myNavbar">
 
