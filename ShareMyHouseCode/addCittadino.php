7aa04d22cd6d6c1614b5ee9399c2d8be641b5b5f
francesca <festa670@gmail.com>
2019-05-14 12:46:10 +0200

    modifiche pagina addcittadino

--- ShareMyHouseCode/addCittadino.php
+++ ShareMyHouseCode/addCittadino.php
@@ -41,12 +41,12 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="">Area Utente</a>
+            <a class="navbar-brand" href="">ShareMyHouse</a>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav bottoniStato">
-                <li role="button" class ="bottoneStato" ><a href="profilo.php">Profilo</a></li>
-                <li role="button" class ="bottoneStato active"><a href="mieimmobili.php">I Miei Immobili</a></li>
+                <li role="button" class ="bottoneStato" ><a href="riepilogo.php">Immobili</a></li>
+                <li role="button" class ="bottoneStato active"><a href="cittadini.php">Cittadini</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="index.php" onclick="logout()"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
@@ -56,11 +56,11 @@
 </nav>
 
 <div class="container-fluid text-center">
-    <h2>Aggiunta Immobile</h2><BR><BR>
+    <h2>Aggiunta Cittadino</h2><BR><BR>
     <div class="container">
         <div class="row">
             <div class="col-md-4"><BR>
-                <h4><i class="fa fa-id-card-o"> Alias Immobile</i></h4>
+                <h4>Codice fiscale</h4>
                 <h5><input id="immAlias" type="text" value=""></h5><BR>
                 <h4><i class="fa fa-wheelchair"> Accesso Disabili</i></h4>
                 <h5><input id="immDisabili" type="checkbox" value=""></h5><BR>
@@ -97,9 +97,9 @@
 
 
 
-                <h4><i class="fa fa-calendar"> Scadenza Disponibilità</i></h4>
-                <h5><input id="immDisponibilita" type="date" placeholder="AAAA-MM-GG " value=""></h5><BR>
-                <h4><i class="fa fa-users"> Posti totali</i></h4>
+                <h4>Città</h4>
+                <h5><input id="immDisponibilita" type="" placeholder="" value=""></h5><BR>
+                <h4>Indirizzo</h4>
                 <h5><input id="immPosti" type="number" min="1" max="50" value=""></h5><BR>
 
             </div>
